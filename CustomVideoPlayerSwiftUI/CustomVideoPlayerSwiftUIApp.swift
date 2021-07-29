2bb51280059e90111dcfaaab5c34af7beefcac9b
Alex Bonney <bonney977@gmail.com>
2021-07-29 12:37:17 +0300

    Initial Commit

---
+++ CustomVideoPlayerSwiftUI/CustomVideoPlayerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomVideoPlayerSwiftUIApp.swift
+//  CustomVideoPlayerSwiftUI
+//
+//  Created by Alexander Bonney on 7/29/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomVideoPlayerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
